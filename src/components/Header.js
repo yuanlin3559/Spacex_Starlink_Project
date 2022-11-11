54ed474b42ede15def5e422d19fd68df71998f20
Yuan Lin <yuanlin3559@gmail.com>
2022-11-10 18:31:52 -0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import starlinkLogo  from '../assets/images/starlink_logo.svg';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={starlinkLogo} className="App-logo" alt="logo" />
+                <p className="title">
+                    StarLink Tracker
+                </p>
+            </header>
+        );
+    }
+}
+
+export default Header;
+
